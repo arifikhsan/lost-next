b504f007c513bba22e81310cf4bb4db2b61f6d2b
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-11-18 14:20:03 +0700

    logo svg

--- components/Header.js
+++ components/Header.js
@@ -6,9 +6,8 @@ export default function Header() {
       <div className="max-w-5xl mx-auto">
         <div className="flex items-center justify-between">
           <Link href="/">
-            <a className="p-4 text-2xl font-bold leading-none no-underline font-display">
-              <span className=" text-primary">bantu</span>
-              <span className="text-accent">temu</span>
+            <a className="p-4">
+              <img src="/logo.svg" alt="logo bantutemu" />
             </a>
           </Link>
           <button className="p-4">Menu</button>
