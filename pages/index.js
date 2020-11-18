8139d7618ef110b850310b01b6100f5032ede3df
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-11-18 10:53:04 +0700

    list item

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,38 @@ import Link from "next/link";
 import Layout from "components/Layout";
 import SEO from "components/Seo";
 import { getSiteMetaData } from "utils/helpers";
+import moment from "moment";
 
-export default function Home() {
+function Home({ data }) {
+  // console.log(data);
   const siteMetadata = getSiteMetaData();
   const metaDescription = siteMetadata.description || "";
   const metaTitle = siteMetadata.titleDense || "";
 
+  const showTime = (timeStart) => {
+    return moment(timeStart).fromNow();
+  };
+
   const bantuTemu = <span className="font-semibold">{metaTitle}</span>;
+
+  const items = (
+    <div className="grid gap-4">
+      {data.map((item) => (
+        <div key={item.id} className="p-4 border rounded">
+          <div className="flex justify-between text-xs">
+            <span className="text-gray-700">{item.condition}</span>
+          </div>
+          <p className="mt-1">{item.title}</p>
+          <div className="mt-4 text-sm text-gray-700">
+            {item.reward > 0 && <p>Hadiah: Rp.{item.reward}</p>}
+            <p>
+              {item.condition} {showTime(item.time_start)}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 
   return (
     <Layout>
@@ -20,7 +45,10 @@ export default function Home() {
             {metaDescription}
           </h1>
         </div>
-        <div>stuff goes brrrr</div>
+        <div>
+          <h2 className="text-2xl">Barang Terlapor</h2>
+          <div className="mt-4">{items}</div>
+        </div>
         <div className="">
           <h1 className="text-2xl text-blue-500 font-display">
             Kehilangan uang, dompet, sim, ktp, atau kawan-kawannya? Segera lapor
@@ -38,3 +66,13 @@ export default function Home() {
     </Layout>
   );
 }
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.API_URL}api/v1/items`);
+  let data = await res.json();
+  data = data["data"];
+
+  return { props: { data } };
+}
+
+export default Home;
