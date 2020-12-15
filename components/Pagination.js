e2f6af974eb7e2c6da7504de2f32470f0ab20abe
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-15 10:31:48 +0700

    fix numbering

--- components/Pagination.js
+++ components/Pagination.js
@@ -57,132 +57,140 @@ export default function Pagination({ pagination }) {
           <span>laporan</span>
         </p>
       </div>
-      <div className="flex justify-between w-full sm:hidden">
-        {pagination.is_first_page ? (
-          <p></p>
-        ) : (
-          <Link href={prevLink()}>
-            <a className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 transition duration-500 bg-white border border-gray-300 rounded-md hover:text-gray-500">
-              Sebelumnya
-            </a>
-          </Link>
-        )}
-        {pagination.is_last_page || pagination.size === 0 ? (
-          <p></p>
-        ) : (
-          <Link href={nextLink()}>
-            <a className="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 transition duration-500 bg-white border border-gray-300 rounded-md hover:text-gray-500">
-              Selanjutnya
-            </a>
-          </Link>
-        )}
-      </div>
-      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-        <div>
-          <nav
-            className="relative z-0 inline-flex -space-x-px shadow-sm"
-            aria-label="Pagination"
-          >
+      {pagination.size > 0 && (
+        <>
+          <div className="flex justify-between w-full sm:hidden">
             {pagination.is_first_page ? (
-              <a
-                href="#"
-                className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 opacity-50 rounded-l-md hover:bg-gray-50"
-              >
-                <span className="sr-only">Previous</span>
-                <svg
-                  className="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
+              <p></p>
             ) : (
               <Link href={prevLink()}>
-                <a className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50">
-                  <span className="sr-only">Previous</span>
-                  <svg
-                    className="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                <a className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 transition duration-500 bg-white border border-gray-300 rounded-md hover:text-gray-500">
+                  Sebelumnya
                 </a>
               </Link>
             )}
-
-            {pageRange().map((pageNumber) => {
-              return (
-                <Link href={`/item?page=${pageNumber}&per=10`} key={pageNumber}>
-                  <a
-                    className={
-                      `relative inline-flex items-center px-4 py-2 text-sm font-medium border border-gray-300 hover:bg-gray-50 ` +
-                      (pagination.current_page == pageNumber
-                        ? `text-secondary bg-primary`
-                        : `text-gray-700 bg-white`)
-                    }
-                  >
-                    {pageNumber}
-                  </a>
-                </Link>
-              );
-            })}
             {pagination.is_last_page || pagination.size === 0 ? (
-              <a
-                href="#"
-                className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 opacity-50 rounded-r-md hover:bg-gray-50"
-              >
-                <span className="sr-only">Next</span>
-                <svg
-                  className="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
+              <p></p>
             ) : (
               <Link href={nextLink()}>
-                <a className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50">
-                  <span className="sr-only">Next</span>
-                  <svg
-                    className="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                <a className="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 transition duration-500 bg-white border border-gray-300 rounded-md hover:text-gray-500">
+                  Selanjutnya
                 </a>
               </Link>
             )}
-          </nav>
-        </div>
-      </div>
+          </div>
+          <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+            <div>
+              <nav
+                className="relative z-0 inline-flex -space-x-px shadow-sm"
+                aria-label="Pagination"
+              >
+                {pagination.is_first_page ? (
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 opacity-50 rounded-l-md hover:bg-gray-50"
+                  >
+                    <span className="sr-only">Previous</span>
+                    <svg
+                      className="w-5 h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                ) : (
+                  <Link href={prevLink()}>
+                    <a className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50">
+                      <span className="sr-only">Previous</span>
+                      <svg
+                        className="w-5 h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </Link>
+                )}
+
+                {pageRange().map((pageNumber) => {
+                  return (
+                    <Link
+                      href={`/item?page=${pageNumber}&per=10`}
+                      key={pageNumber}
+                    >
+                      <a
+                        className={
+                          `relative inline-flex items-center px-4 py-2 text-sm font-medium border border-gray-300 hover:bg-gray-50 ` +
+                          (pagination.current_page == pageNumber
+                            ? `text-secondary bg-primary`
+                            : `text-gray-700 bg-white`)
+                        }
+                      >
+                        {pageNumber}
+                      </a>
+                    </Link>
+                  );
+                })}
+
+                {pagination.is_last_page || pagination.size === 0 ? (
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 opacity-50 rounded-r-md hover:bg-gray-50"
+                  >
+                    <span className="sr-only">Next</span>
+                    <svg
+                      className="w-5 h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                ) : (
+                  <Link href={nextLink()}>
+                    <a className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50">
+                      <span className="sr-only">Next</span>
+                      <svg
+                        className="w-5 h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </Link>
+                )}
+              </nav>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
